ff94e7e2804cb9150cd57f995ddcc3297d091b19
Kazuya Seto <jrt1900@gmai.com>
2016-06-24 04:04:52 +0900

    バルネアプリ、モデル部分作成途中 テストの作成途中

---
+++ LetsSplitTheBill/CheckModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace LetsSplitTheBill
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+
+		public static void print()
+		{
+			System.Diagnostics.Debug.WriteLine("test");
+		}
+	}
+}
+
